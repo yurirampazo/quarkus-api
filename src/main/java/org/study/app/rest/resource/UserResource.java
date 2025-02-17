127c52ae743321b8f4a4aae7745980599e8e8258
yuri <ymrampazo@gmail.com>
2025-02-16 23:24:51 -0300

    Implemented update and delete

--- src/main/java/org/study/app/rest/resource/UserResource.java
+++ src/main/java/org/study/app/rest/resource/UserResource.java
@@ -7,6 +7,9 @@ import jakarta.ws.rs.core.Response;
 import org.study.app.domain.model.User;
 import org.study.app.rest.dto.CreateUserRequestDTO;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -32,5 +35,38 @@ public class UserResource {
     @GET
     public Response listAllUsers() {
         return Response.ok(User.findAll().list()).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteUser(@PathParam("id") Long id) {
+        var response = (User) User.findById(id);
+
+        if (Objects.nonNull(response)) {
+            response.delete();
+            return Response.noContent().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateUser(@PathParam("id") Integer id, CreateUserRequestDTO requestDTO) {
+
+        User user = User.findById(id);
+
+        if (user != null) {
+            user.setName(isNullOrEmpty(requestDTO.getName()) ? user.getName() : requestDTO.getName());
+            user.setAge(requestDTO.getAge() != null ? requestDTO.getAge() : user.getAge());
+            return Response.noContent().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+
+    public static boolean isNullOrEmpty(String text) {
+        return text != null && !text.isBlank();
     }
 }
