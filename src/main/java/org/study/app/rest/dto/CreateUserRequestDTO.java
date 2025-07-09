86a988370799820b463deb2653a5d86aa79399b1
yuri <ymrampazo@gmail.com>
2025-07-09 20:38:14 -0300

    Implemented lombok dependency, removed boilerplate code

--- src/main/java/org/study/app/rest/dto/CreateUserRequestDTO.java
+++ src/main/java/org/study/app/rest/dto/CreateUserRequestDTO.java
@@ -2,36 +2,17 @@ package org.study.app.rest.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserRequestDTO {
 
   @NotBlank(message = "Name is required")
   private String name;
   @NotNull(message = "Age is required")
   private Integer age;
-
-
-  public CreateUserRequestDTO() {
-  }
-
-  public CreateUserRequestDTO(String name, Integer age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Integer getAge() {
-    return age;
-  }
-
-  public void setAge(Integer age) {
-    this.age = age;
-  }
 }
--- src/main/java/org/study/app/rest/dto/FieldError.java
+++ src/main/java/org/study/app/rest/dto/FieldError.java
@@ -1,27 +1,13 @@
 package org.study.app.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FieldError {
     private String field;
     private String message;
-
-    public FieldError(String field, String message) {
-        this.field = field;
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getField() {
-        return field;
-    }
-
-    public void setField(String field) {
-        this.field = field;
-    }
 }
